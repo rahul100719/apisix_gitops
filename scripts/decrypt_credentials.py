9cc4154e084980c31b8942083aca4073f6fd1f63
Rahul <46256465+rahul100719@users.noreply.github.com>
2025-11-16 17:32:46 +0530

    Update decrypt_credentials.py

--- scripts/decrypt_credentials.py
+++ scripts/decrypt_credentials.py
@@ -1,8 +1,7 @@
 import os
-from cryptography.fernet import Fernet
 
-key = open("../apisix_secret.key", "rb").read()
-fernet = Fernet(key)
+
+
 
 # Check if Jenkins passed encrypted credentials
 jenkins_clients = os.environ.get("ENCRYPTED_CLIENTS_Rahul")
