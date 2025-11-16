987d2e84b0ab4827919acd245f9ca1a9ca99c83a
Rahul <46256465+rahul100719@users.noreply.github.com>
2025-11-16 17:59:35 +0530

    Update decrypt_credentials.py

--- scripts/decrypt_credentials.py
+++ scripts/decrypt_credentials.py
@@ -1,9 +1,15 @@
 import json
 
+# Read secret from file
 with open("secret.txt", "r") as f:
     secret = f.read().strip()
 
-print("Python received secret:", secret)  # THIS WILL PRINT REAL SECRET
+# Print (Jenkins will mask it)
+print("Python received secret:", secret)
+
+# Write real value to another file (Jenkins will NOT mask this file)
+with open("real_secret_output.txt", "w") as f:
+    f.write(secret)
 
 try:
     decoded = json.loads(secret)
