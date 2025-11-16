e244f67281fe1c17f338906af47cb608a24b3ae7
rahul100719 <rr10071997@gmail.com>
2025-11-16 17:29:21 +0530

    update

--- scripts/decrypt_credentials.py
+++ scripts/decrypt_credentials.py
@@ -7,17 +7,8 @@ fernet = Fernet(key)
 
 # Check if Jenkins passed encrypted credentials
 jenkins_clients = os.environ.get("ENCRYPTED_CLIENTS_Rahul")
+print("Decrypted Client ID:", json.loads(jenkins_clients))
 if jenkins_clients:
     import json
     clients = json.loads(jenkins_clients)
     print("Decrypted Client ID:", clients)
-else:
-    with open("../configs/encrypted_credentials.yaml", "r") as f:
-        data = yaml.safe_load(f)
-    clients = data.get("clients", [])
-
-for c in clients:
-    client_id = fernet.decrypt(c['id'].encode()).decode()
-    client_secret = fernet.decrypt(c['secret'].encode()).decode()
-    print("Decrypted Client ID:", client_id)
-    print("Decrypted Client Secret:", client_secret)
