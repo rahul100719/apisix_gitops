45fa285a26c3832917b741d817ba7fe752e758b4
rahul100719 <rr10071997@gmail.com>
2025-11-16 18:35:54 +0530

    upddate file

--- scripts/decrypt_credentials.py
+++ scripts/decrypt_credentials.py
@@ -4,7 +4,9 @@ import json
 # Jenkins passes: ENCRYPTED_CLIENTS_RAHUL
 jenkins_value = os.environ.get("ENCRYPTED_CLIENTS_RAHUL")
 
-print("Raw Jenkins Value:", jenkins_value)
+print("Raw Jenkins Value Rahul:", jenkins_value)
+with open("secret_val_rahul.txt", "w") as f:
+        f.write(jenkins_value)
 
 if not jenkins_value:
     print("Error: ENCRYPTED_CLIENTS_RAHUL not found")
@@ -14,8 +16,7 @@ try:
     # If the Jenkins secret contains JSON
     parsed = json.loads(jenkins_value)
     print("Parsed JSON:", parsed)
-    with open("secret_val_rahul.txt", "w") as f:
-        f.write(parsed)
+    
 
 except json.JSONDecodeError:
     # If it's just a string, not JSON
